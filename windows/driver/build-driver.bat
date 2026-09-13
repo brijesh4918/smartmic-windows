fd20753e77eec0eff1a88ee598f2fdbcf91fcfb8
brijesh4918 <49075804+brijesh4918@users.noreply.github.com>
2026-09-12 22:56:27 -0400

    Override InfVerif, Inf2Cat, and DriverPackageTarget completely in the .vcxproj file to stop MSBuild from running them

--- windows/driver/build-driver.bat
+++ windows/driver/build-driver.bat
@@ -9,7 +9,7 @@ setlocal
 
 echo.
 echo === 1/4  Building the driver =================================================
-msbuild SmartMicDriver.vcxproj /p:Configuration=Release /p:Platform=x64 /m /p:RunInfVerif=false /p:InfVerif_Enable=false /p:RunInf2Cat=false /p:Inf2Cat_Enable=false
+msbuild SmartMicDriver.vcxproj /p:Configuration=Release /p:Platform=x64 /m
 if errorlevel 1 goto :failed
 
 set OUT=x64\Release\smartmic
